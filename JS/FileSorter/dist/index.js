4a7e592cf20dfb0868ecfe5da095c7721a38aaa1
William AbouSharaf <liams_1998@hotmail.com>
2020-09-13 02:29:17 +0300

    Created file sorter program

---
+++ JS/FileSorter/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Core_1 = require("./Core");
+console.log("running...");
+Core_1.Controllers.HandleSortFiles();
+console.log("Done.");
+//# sourceMappingURL=index.js.map
---
+++ JS/FileSorter/src/index.ts
@@ -0,0 +1,7 @@
+import {Controllers} from "./Core";
+
+console.log("running...")
+
+Controllers.HandleSortFiles();
+
+console.log("Done.");
